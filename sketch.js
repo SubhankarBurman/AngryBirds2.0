8546f252524117971a68bef587860d6e82205b5d
SubhankarBurman <subhankarburman123@gmail.com>
2020-12-16 15:43:04 +0530

    Added a new level of boxes

--- sketch.js
+++ sketch.js
@@ -26,13 +26,19 @@ function setup(){
 
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
-    pig3 = new Pig(810, 220);
+    pig2 = new Pig(810, 220);
 
     log3 =  new Log(810,180,300, PI/2);
 
-    box5 = new Box(810,160,70,70);
-    log4 = new Log(760,120,150, PI/7);
-    log5 = new Log(870,120,150, -PI/7);
+    box5 = new Box(810,80,70,70);
+    log4 = new Log(760,50,150, PI/7);
+    log5 = new Log(870,50,150, -PI/7);
+
+    box6 = new Box(700,160,70,70);
+    box7 = new Box(920,160,70,70);
+    pig3 = new Pig(810,130);
+    log6 = new Log(810,120,300,PI/2);
+
 
     bird = new Bird(100,100);
 
@@ -52,12 +58,17 @@ function draw(){
 
     box3.display();
     box4.display();
-    pig3.display();
+    pig2.display();
     log3.display();
 
     box5.display();
     log4.display();
     log5.display();
+
+    box6.display();
+    box7.display();
+    pig3.display();
+    log6.display();
 
     bird.display();
     platform.display();
